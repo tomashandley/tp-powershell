c257d1e78fd159c37829bd04f533a9381e743849
Tomas Handley <tomashandley@hotmail.com>
2019-10-13 04:36:36 -0300

    Ejercicio 5 logica ok. Falta Get-Help

--- Ejercicio5/ejercicio5.ps1
+++ Ejercicio5/ejercicio5.ps1
@@ -26,12 +26,21 @@
 
 #Validaciones
 Param(
-    [Parameter(Mandatory = $true, ParameterSetName = "Procesos")]
-    [switch]
-    $procesos
+   [Parameter(Mandatory = $true, ParameterSetName = "Procesos")]
+   [switch]
+   $procesos,
+
+   [Parameter(Mandatory = $true, ParameterSetName = "Peso")]
+   [Parameter(Mandatory = $false, ParameterSetName = "Directorio")]
+   [switch]
+   $peso,
+
+   [Parameter(Mandatory = $false, ParameterSetName = "Directorio")]
+   [string]
+   $directorio
 )
 
-#infinite loop for calling connect function  
+#Loop infito que al realizar una operacion, queda esperando 3 segundos  
 while(1) {
     if($procesos) {
         Write-Host "#### Cantidad de procesos corriendo ####"
@@ -39,7 +48,41 @@ while(1) {
         Write-Host "$cantidadDeProcesos"
         Write-Host "########################################"
     } else {
-        
+         if($peso) {
+
+            #Obtengo el tamanio de un directorio en Bytes   
+            $pesoDirectorio = "{0}" -f ((Get-ChildItem $directorio -Recurse -Force | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum)
+
+            Write-Host "Peso Directorio en Bytes: $pesoDirectorio"
+
+            if([System.Numerics.BigInteger]$pesoDirectorio -ge 1000000000) {
+               #Muestro el tamanio en GB
+               $pesoDirectorioMb=$pesoDirectorio / 1Gb;
+               #Redondeo a 2 digitos
+               $pesoDirectorioRedondeado=[math]::Round($pesoDirectorioMb,2); 
+               Write-Host "$pesoDirectorioRedondeado GB"
+            } else {
+               if([System.Numerics.BigInteger]$pesoDirectorio -ge 1000000) {
+                  #Muestro el tamanio en MB
+                  $pesoDirectorioMb=$pesoDirectorio / 1Mb;
+                  #Redondeo a 2 digitos
+                  $pesoDirectorioRedondeado=[math]::Round($pesoDirectorioMb,2); 
+                  Write-Host "$pesoDirectorioRedondeado MB"
+               }else {
+                  #Muestro el tamanio en KB
+                  $pesoDirectorioMb=$pesoDirectorio / 1Kb;
+                  #Redondeo a 2 digitos
+                  $pesoDirectorioRedondeado=[math]::Round($pesoDirectorioMb,2); 
+                  Write-Host "$pesoDirectorioRedondeado KB"   
+               }
+            }
+
+            # $pesoDirectorioMb=$pesoDirectorio / 1Kb;
+            # $pesoDirectorioRedondeado=[math]::Round($pesoDirectorioMb,2); 
+            # Write-Host "#### Tamanio directorio: $directorio ####"
+            # Write-Host "$pesoDirectorioRedondeado KB"
+            Write-Host "#########################################"
+        }
     }
-    start-sleep -seconds 1
+    start-sleep -seconds 3
 }
